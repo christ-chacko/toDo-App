260a254d3770da7f6fad1ca241be1d4022be9e31
christ-chacko <christchacko@gmail.com>
2025-10-10 20:45:23 +0530

    [Project Completed]

---
+++ context/ThemeContext.js
@@ -0,0 +1,21 @@
+import { createContext, useState } from "react";
+import { Appearance } from "react-native";
+import { Colors } from "../constants/Colors";
+
+export const ThemeContext = createContext({})
+
+export const ThemeProvider = ({ children }) => {
+    const [colorScheme, setcolorScheme] = useState(Appearance.getColorScheme() || 'light')
+
+    const theme = colorScheme === 'dark'
+        ? Colors.dark
+        : Colors.light
+
+    return(
+        <ThemeContext.Provider value = {{
+            colorScheme, setcolorScheme, theme
+        }}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
---
+++ data/todos.js
@@ -0,0 +1,4 @@
+
+export const data = [
+  { "id": 1, "title": "This is an example", "completed": false },
+]
